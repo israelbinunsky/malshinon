6afa1709d88421855c3d6686dfea5c4ba23aaf29
israelb <israelbinunsky@gmail.com>
2025-06-09 16:44:45 +0300

    class reporter identification

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace malshinon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dal dal = new Dal();
+            dal.Identification();
+        }
+    }
+}
